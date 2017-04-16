e4c45f17cac95f008d5f84d15d6b5c155fbd5a48
KhAn <KhAn>
2017-04-16 14:39:36 +0530

    Package view, review viewand blog view added

--- addreviews.php
+++ addreviews.php
@@ -6,26 +6,25 @@
 <body>
 <?php
 		include_once('home/catalog/connect.khan');
+		include_once('home/components/val.php');
+		 //var $__ValFlag; val tbr
 ?>
 
 
 <?php
 
 if(isset($_POST['btn'])){ 
-		$email=	$_POST['btn'];
-					
-					$name=mysql_real_escape_string(trim(strip_tags(stripslashes($_POST['name']))));
-					$about=mysql_real_escape_string(trim(strip_tags(stripslashes($_POST['about']))));	;
-					$desc=mysql_real_escape_string(trim(strip_tags(stripslashes($_POST['desc']))));	;					
-					$query="INSERT INTO `reviews`( `name`, `about`, `desc`) VALUES ('$name', '$about', '$desc');";
-					
-					if(mysqli_query($dbconn,$query) or die( mysqli_error())){
-						echo'<div style="width:80%;padding-top:50px; padding-bottom:60px; padding-left:20%; overflow:hidden; color:green; font-size:32px; background:#999999;">Review added</div>';
-					}
-					else{	
-						echo '<div style="width:80%;padding-top:50px; padding-bottom:60px; padding-left:20%; overflow:hidden; color:red; font-size:32px; 		background:#999999;">Something went wrong</div>';
-					}
-				
+		$name=mysql_real_escape_string(trim(strip_tags(stripslashes($_POST['name']))));
+		$about=mysql_real_escape_string(trim(strip_tags(stripslashes($_POST['about']))));	;
+		$desc=mysql_real_escape_string(trim(strip_tags(stripslashes($_POST['desc']))));	;					
+		$query="INSERT INTO `reviews`( `name`, `about`, `desc`) VALUES ('$name', '$about', '$desc');";
+		
+		if(mysqli_query($dbconn,$query) or die( mysqli_error())){
+			echo'<div style="width:80%;padding-top:50px; padding-bottom:60px; padding-left:20%; overflow:hidden; color:green; font-size:32px; background:#999999;">Review added</div>';
+		}
+		else{	
+			echo '<div style="width:80%;padding-top:50px; padding-bottom:60px; padding-left:20%; overflow:hidden; color:red; font-size:32px; 		background:#999999;">Something went wrong</div>';
+		}				
 }
 ?>
 
